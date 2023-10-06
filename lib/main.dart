487bc439ca1dfd0878d3810c04382066ed662ce9
Sinx <gabriel.jbrito2@gmail.com>
2023-10-06 17:28:19 -0300

    Testando widgets basicos

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,83 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: //CLICANDO ALT + ENTER EM CIMA DA CLASSE VC PODE FAZER COM QUE ELA TENHA OUTRAS FUNÇÕES, COMO SER FILHO DE UMA CLASSE MAIOR.
+          Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(color: Colors.cyan, height: 50, width: 50),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber, height: 30, width: 300,
+              //utilizando texto
+              child: Text(
+                'Oi eu sou o Sinx',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
 
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.red, width: 100, height: 100,),
-              Container(color: Colors.blue, width: 50, height: 50,),
-            ],
-
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(color: Colors.blue, width: 100, height: 100,),
-              Container(color: Colors.red, width: 50, height: 50,),
-            ],
-
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment:  CrossAxisAlignment.center,
-            children: [
-              Container(color:  Colors.cyan, height: 50, width: 50),
-              Container(color:  Colors.pinkAccent, height: 50, width: 50,),
-              Container(color: Colors.purple, height: 50, width: 50,),
-            ],
-          )
-
-        ],
-      ) ,
+            //Utilizando Botão
+            ElevatedButton(
+                onPressed: () {
+                  print('Você Apertou o Botão');
+                },
+                child: Text('Aperte o botão!'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
