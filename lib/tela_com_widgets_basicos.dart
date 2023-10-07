7123068f83a380e1107fe5d22fdfc6d1eae36df3
Sinx <gabriel.jbrito2@gmail.com>
2023-10-07 11:00:30 -0300

    Testando widgets basicos

---
+++ lib/tela_com_widgets_basicos.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+//Função principal de um projeto Flutter
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: //CLICANDO ALT + ENTER EM CIMA DA CLASSE VC PODE FAZER COM QUE ELA TENHA OUTRAS FUNÇÕES, COMO SER FILHO DE UMA CLASSE MAIOR.
+      Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(color: Colors.cyan, height: 50, width: 50),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+
+              //utilizando texto
+              child: const Text(
+                'Oi eu sou o Sinx',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    decoration: TextDecoration.none
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            //Utilizando Botão
+            ElevatedButton(
+              onPressed: () {
+                print('Você Apertou o Botão');
+              },
+              child: const Text('Aperte o botão!'),
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
